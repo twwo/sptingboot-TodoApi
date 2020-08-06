209d8b41811308bcea557e130079548250407c23
LISH5 <727904507@qq.com>
2020-08-06 20:16:51 +0800

    [TodoAPI][Shao] test: add and pass should_delete_todo_when_hit_delete_todo_given_id and should_update_todo_when_hit_update_todo_given_todo_info_and_id and add afterEach to clear database

--- src/test/java/com/OOCL/Todo/integration/TodoIntegrationTest.java
+++ src/test/java/com/OOCL/Todo/integration/TodoIntegrationTest.java
@@ -2,16 +2,13 @@ package com.OOCL.Todo.integration;
 
 import com.OOCL.Todo.model.Todo;
 import com.OOCL.Todo.repository.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +26,11 @@ public class TodoIntegrationTest {
     private TodoRepository todoRepository;
 
     private final Todo testTodo = new Todo(null, "todo1", false);
+
+    @AfterEach
+    private void afterEach() {
+        todoRepository.deleteAll();
+    }
 
     @Test
     void should_return_todoList_when_hit_getTodoList_given_none() throws Exception {
@@ -57,6 +59,40 @@ public class TodoIntegrationTest {
                 .andExpect(status().isCreated());
         Todo addedTodo = todoRepository.findAll().get(0);
         assertEquals("test3", addedTodo.getContent());
-        assertEquals(false, addedTodo.getStatus());
+        assertFalse(addedTodo.getStatus());
+    }
+
+    @Test
+    void should_update_todo_when_hit_update_todo_given_todo_info_and_id() throws Exception {
+        //given
+        Todo addedTodo = todoRepository.save(testTodo);
+        System.out.println(addedTodo);
+        String updateTodoInfo = "{\n" +
+                "    \"id\": " + addedTodo.getId() + ",\n" +
+                "    \"content\" : \"test1\",\n" +
+                "    \"status\" : true\n" +
+                "}";
+
+        //when
+        mockMvc.perform(put("/todos/" + addedTodo.getId()).contentType(MediaType.APPLICATION_JSON).content(updateTodoInfo))
+                .andExpect(status().isOk());
+
+        Todo updatedTodo = todoRepository.findById(addedTodo.getId()).orElse(null);
+        assertNotNull(updatedTodo);
+        assertEquals(addedTodo.getId(), updatedTodo.getId());
+        assertEquals("test1", updatedTodo.getContent());
+        assertTrue(updatedTodo.getStatus());
+    }
+
+    @Test
+    void should_delete_todo_when_hit_delete_todo_given_id() throws Exception {
+        //given
+        Todo addedTodo = todoRepository.save(testTodo);
+
+        //when
+        mockMvc.perform(delete("/todos/" + addedTodo.getId()))
+                .andExpect(status().isOk());
+        Todo deletedTodo = todoRepository.findById(addedTodo.getId()).orElse(null);
+        assertNull(deletedTodo);
     }
 }
