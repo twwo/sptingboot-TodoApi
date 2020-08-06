69806e0861cf775d9b53276438acb44e817588bc
LISH5 <727904507@qq.com>
2020-08-06 08:37:34 +0800

    [TodoAPI][Shao] feature: add model Todo

---
+++ src/main/java/com/OOCL/Todo/model/Todo.java
@@ -0,0 +1,21 @@
+package com.OOCL.Todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    private Boolean status;
+}
+
